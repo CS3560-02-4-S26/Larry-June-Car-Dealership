0c02e1277e8e9b58ef397571c45d0d12c6785115
Avery King <gperson@disroot.org>
2026-04-15 11:41:26 -0700

    Cleanup source tree
    - Drop 'target' folder
    - Merge 'larry-june-dealership' subfolder with the main project

--- larry-june-dealership/larry-june-dealership/src/main/java/com/larryjune/dealership/model/Customer.java
+++
@@ -1,9 +0,0 @@
-package com.larryjune.dealership.model;
-
-public class Customer extends Account {
-
-    public Customer(int accountID, String firstName, String lastName, String email, String phoneNum,
-            String shippingAddress) {
-        super(accountID, firstName, lastName, email, phoneNum, shippingAddress);
-    }
-}
