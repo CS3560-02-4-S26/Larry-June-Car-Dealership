62113ae776493fd82b52c3efc907f3b58d7ed7d1
Avery King <gperson@disroot.org>
2026-04-20 11:50:13 -0700

    UI: Initial dynamic item loading
    Everything is still pretty hard-coded, but this is a demonstration that loading a car item works.
    
    A more refined UI is still to come.

--- src/main/java/com/larryjune/dealership/App.java
+++ src/main/java/com/larryjune/dealership/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -11,14 +12,27 @@ public class App extends Application {
     @Override
     //Starts the main Application and loads ManScree.Fxml
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(
+        // Load all FXML resources
+        FXMLLoader mainScreenLoader = new FXMLLoader(
             getClass().getResource("/com/larryjune/dealership/MainScreen.fxml")
         );
-        Parent root = loader.load();
 
-        Scene scene = new Scene(root, 1000, 900);
+        FXMLLoader carItemLoader = new FXMLLoader(
+                getClass().getResource("/com/larryjune/dealership/CarItem.fxml")
+        );
+
+        // Load the main screen and create a scene for it
+        VBox root = mainScreenLoader.load();
+        Scene mainScene = new Scene(root);
+
+        // Load a car item card and add it to the main scene
+        Parent carItem = carItemLoader.load();
+        VBox carItemLayout = new VBox(carItem);
+        root.getChildren().add(carItemLayout);
+
+        // Setup our stage and show everything
         stage.setTitle("Larry June Dealership");
-        stage.setScene(scene);
+        stage.setScene(mainScene);
         stage.show();
     }
 
