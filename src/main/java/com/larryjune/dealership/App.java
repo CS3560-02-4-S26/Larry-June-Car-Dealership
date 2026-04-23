41203496faa07ef1193972234c384fab7413cbc1
Avery King <gperson@disroot.org>
2026-04-22 20:59:44 -0700

    Finish implementing dummy dynamic vehicles UI
    All the options don't do anything quite yet, and they are all dummy elements. but the UI is a proof-of-concept in that it works.

--- src/main/java/com/larryjune/dealership/App.java
+++ src/main/java/com/larryjune/dealership/App.java
@@ -2,13 +2,11 @@ package com.larryjune.dealership;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
-
     @Override
     //Starts the main Application and loads ManScree.Fxml
     public void start(Stage stage) throws Exception {
@@ -17,18 +15,9 @@ public class App extends Application {
             getClass().getResource("/com/larryjune/dealership/MainScreen.fxml")
         );
 
-        FXMLLoader carItemLoader = new FXMLLoader(
-                getClass().getResource("/com/larryjune/dealership/CarItem.fxml")
-        );
-
         // Load the main screen and create a scene for it
         VBox root = mainScreenLoader.load();
         Scene mainScene = new Scene(root);
-
-        // Load a car item card and add it to the main scene
-        Parent carItem = carItemLoader.load();
-        VBox carItemLayout = new VBox(carItem);
-        root.getChildren().add(carItemLayout);
 
         // Setup our stage and show everything
         stage.setTitle("Larry June Dealership");
--- src/main/java/com/larryjune/dealership/MainScreenController.java
+++ src/main/java/com/larryjune/dealership/MainScreenController.java
@@ -2,14 +2,42 @@ package com.larryjune.dealership;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainScreenController {
-        //Handles the login button in Main Screen
+    @FXML
+    public TilePane carGrid;
+
+    @FXML
+    public void initialize() {
+        // Add dummy car items
+        FXMLLoader carItemLoader;
+        for (int i = 0; i < 20; i++) {
+            try {
+                carItemLoader = new FXMLLoader(
+                        getClass().getResource("/com/larryjune/dealership/CarItem.fxml")
+                );
+
+                VBox carItem = carItemLoader.load();
+                ((Label) carItem.getChildren().get(1)).setText("Car " + (i + 1));
+
+                carGrid.getChildren().add(carItem);
+            } catch (IOException e) {
+                // TODO: Better error handling
+                System.err.println("ERROR: Failed to load cars!");
+            }
+        }
+    }
+
+    //Handles the login button in Main Screen
     public void handleLogin(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(
             getClass().getResource("/com/larryjune/dealership/LoginScreen.fxml")
