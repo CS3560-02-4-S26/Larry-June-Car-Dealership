ae4e2f2adb1b354d3d490a4102f062d88224ba94
Avery King <gperson@disroot.org>
2026-04-29 00:41:49 -0700

    CarItemController: Improve image loading
    - Set the default car image to a throbber GIF
    - Switch the image to "No Image Available" on failure
    - Move image loading to a background thread for each car item

--- src/main/java/com/larryjune/dealership/CarItemController.java
+++ src/main/java/com/larryjune/dealership/CarItemController.java
@@ -3,11 +3,11 @@ package com.larryjune.dealership;
 import com.larryjune.dealership.model.DBControl;
 import com.larryjune.dealership.model.Image;
 import com.larryjune.dealership.model.Vehicle;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-
-import java.util.List;
 
 public class CarItemController {
     @FXML
@@ -23,18 +23,71 @@ public class CarItemController {
     private Label carDescription;
 
     public void setInfo(Vehicle vehicleInfo) {
-        // TODO: Move image loading into a background thread. Loading them here slows the program down
-        try {
-            Image vehicleImage = DBControl.fetchImagesAt(
-                "vehicleID", Integer.toString(vehicleInfo.getVehicleID())
-            ).getFirst();
-            carImage.setImage(new javafx.scene.image.Image(vehicleImage.getImagePath()));
-        } catch (IndexOutOfBoundsException e) {
-          System.err.println("No available vehicle images for vehicleID = " + vehicleInfo.getVehicleID());
-        } catch (Exception e) {
-            System.err.println("Failed to load image with vehicleID = " + vehicleInfo.getVehicleID());
-            e.printStackTrace();
-        }
+        // Load the vehicle image on a background thread and then
+        Thread imageLoadingThread = new Thread(
+            new Task<Boolean>() {
+                @Override
+                public Boolean call() {
+                    try {
+                        // Fetch the vehicle image
+                        Image vehicleImage = DBControl.fetchImagesAt(
+                            "vehicleID", Integer.toString(vehicleInfo.getVehicleID())
+                        ).getFirst();
+
+                        // If we reach this point, set the image back on the main UI thread
+                        Platform.runLater(
+                            new Task<Boolean>() {
+                                @Override
+                                public Boolean call() {
+                                    carImage.setImage(new javafx.scene.image.Image(vehicleImage.getImagePath()));
+                                    return true;
+                                }
+                            }
+                        );
+                    } catch (IndexOutOfBoundsException e) {
+                        // Generally OK, no available images to show
+                        System.err.println("No available vehicle images for vehicleID = " + vehicleInfo.getVehicleID());
+                        Platform.runLater(
+                            new Task<Void>() {
+                                @Override
+                                public Void call() {
+                                    carImage.setImage(
+                                        new javafx.scene.image.Image(
+                                            getClass().getResource("/com/larryjune/dealership/NoImageAvailable.png").toString()
+                                        )
+                                    );
+
+                                    return null;
+                                }
+                            }
+                        );
+                    } catch (Exception e) {
+                        // Some other error occurred, show the details.
+                        System.err.println("Failed to load image with vehicleID = " + vehicleInfo.getVehicleID());
+                        e.printStackTrace();
+                        Platform.runLater(
+                            new Task<Void>() {
+                                @Override
+                                public Void call() {
+                                    carImage.setImage(
+                                        new javafx.scene.image.Image(
+                                            getClass().getResource("/com/larryjune/dealership/NoImageAvailable.png").toString()
+                                        )
+                                    );
+
+                                    return null;
+                                }
+                            }
+                        );
+                        return false;
+                    }
+
+                    return true;
+                }
+            }
+        );
+
+        imageLoadingThread.start();
 
         carTitle.setText(
             vehicleInfo.getYear() + " " + vehicleInfo.getMake() + " " +
