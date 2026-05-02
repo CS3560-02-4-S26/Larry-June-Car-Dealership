e167827cbe0581b5783da5c43134afebb688e6b1
Avery King <gperson@disroot.org>
2026-05-02 00:32:06 -0700

    Manager UI: Implement proper fetching
    Does not get images quite yet, but all other info is retrieved from the
    database.

--- src/main/java/com/larryjune/dealership/Add.java
+++ src/main/java/com/larryjune/dealership/Add.java
@@ -1,25 +1,25 @@
 package com.larryjune.dealership;
 
-import java.io.IOException;
-import java.util.List;
-
+import com.larryjune.dealership.model.DBControl;
+import com.larryjune.dealership.model.Vehicle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.scene.Node;
 
-import com.larryjune.dealership.model.DBControl;
-import com.larryjune.dealership.model.Vehicle;
+import java.io.IOException;
+import java.util.List;
 
 public class Add {
     @FXML
-    private FlowPane carItems;
+    private GridPane carItems;
 
     @FXML
     private Button vehicle;
@@ -27,13 +27,45 @@ public class Add {
     private Button employee;
 
     public void initialize() {
-        // TODO: Fetch all vehicle items from the database
         try {
             List<Vehicle> vehicleData = DBControl.fetchVehicleData();
-            carItems.getChildren().add(new Label("Successfully fetched vehicles (TODO: UI)"));
+            Vehicle vehicle;
+            for (int i = 0; i < vehicleData.size(); i++) {
+                vehicle = vehicleData.get(i);
+
+                // TODO: Improve management UI
+//                try {
+//                    ImageView image = new ImageView(
+//                        new javafx.scene.image.Image(
+//                            DBControl.fetchImagesAt("vehicleID", Integer.toString(vehicle.getVehicleID())).getFirst().getImagePath(),
+//                            true
+//                        )
+//                    );
+//
+//                    carItems.add(image, 0, i);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                    carItems.add(new Label("N/A"), 0, i);
+//                }
+
+                carItems.add(new Label("N/A"), 0, i);
+
+                Label makeModel = new Label(vehicle.getMake() + " " + vehicle.getModel());
+                Label modelYear = new Label(Integer.toString(vehicle.getYear()));
+                Label plate = new Label(vehicle.getVinNumber());
+                Label status = new Label(vehicle.getCarStatus());
+                Label mileage = new Label(Integer.toString(vehicle.getMileage()));
+
+                carItems.add(makeModel, 1, i);
+                carItems.add(modelYear, 2, i);
+                carItems.add(plate, 3, i);
+                carItems.add(status, 4, i);
+                carItems.add(mileage, 5, i);
+            }
         } catch (Exception e) {
+            e.printStackTrace();
             carItems.getChildren().add(
-                new Label("Failed to fetch vehicles (unimplemented)")
+                new Label("Failed to fetch vehicles")
             );
         }
     }
@@ -41,7 +73,7 @@ public class Add {
     @FXML
     private void goBack(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("/com/larryjune/dealership/LoginScreen.fxml"));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).   getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
 
